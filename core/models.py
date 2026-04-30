4fed8c7b1151cdf35af54e79498a80c599606d97
Felipe Droguett <felipedroguett18@gmail.com>
2026-04-29 22:55:27 -0400

    Reinicio de estructura limpia desde respaldo

---
+++ core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+class Categoria(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+
+class ReglaAutomatica(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    palabra_clave = models.CharField(max_length=50)
+    categoria_asignada = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+class Transaccion(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    monto = models.IntegerField()
+    tipo = models.CharField(max_length=10, choices=[('ingreso', 'Ingreso'), ('gasto', 'Gasto')])
+    descripcion = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    raw_text = models.TextField(blank=True, null=True)
